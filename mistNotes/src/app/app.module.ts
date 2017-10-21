f65f685090983f6988f4028aa8984c6c52d311c9
Mistariel <cindariel@gmail.com>
2017-10-21 20:25:21 -0300

    Including components for Angular Material

--- mistNotes/src/app/app.module.ts
+++ mistNotes/src/app/app.module.ts
@@ -4,20 +4,54 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'; // <-- NgModel lives here
 import { HttpModule } from '@angular/http';
 import { AngularFontAwesomeModule } from 'angular-font-awesome/angular-font-awesome';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, 
+          MatAutocompleteModule, 
+          MatInputModule, 
+          MatExpansionModule,
+          MatSelectModule,
+          MatCardModule,
+          MatIconModule,
+          MatRadioModule
+      } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { FooterComponent } from './footer/footer.component';
+import { StartComponent } from './start/start.component';
+
+const routes: Routes = [
+  { path: 'start', component: StartComponent },
+  { path: '',
+    redirectTo: '/start',
+    pathMatch: 'full'
+  }
+];
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    FooterComponent
+    FooterComponent,
+    StartComponent
   ],
   imports: [
+    RouterModule.forRoot(routes, {
+      enableTracing: false
+    }),
     BrowserModule,
-    AngularFontAwesomeModule
+    RouterModule,
+    AngularFontAwesomeModule,
+    BrowserAnimationsModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatCardModule,
+    MatExpansionModule,
+    MatIconModule,
+    MatInputModule,
+    MatRadioModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
