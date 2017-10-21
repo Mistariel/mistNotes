d6e1edc1be5ca76b89843aa1716b72b618320be2
Taconvino <taconvino@gmail.com>
2017-10-21 22:13:20 +0000

    First commit

---
+++ mistNotes/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { Routes, RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { HttpModule } from '@angular/http';
+import { AngularFontAwesomeModule } from 'angular-font-awesome/angular-font-awesome';
+
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AngularFontAwesomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
